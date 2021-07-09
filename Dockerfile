2a0c77dd052d8650ebb752bafa5b0621545a0e05
Lee Myring <mail@thinkstack.io>
2021-07-09 20:53:43 +0100

    feat: add poetry install (#18)

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ ARG PYTHON_VERSION=3.9.5
 FROM python:${PYTHON_VERSION}-alpine
 
 ENV PYTHONFAULTHANDLER=1
+ARG POETRY_VERSION
 ARG TERRAFORM_VERSION
 ARG TERRAGRUNT_VERSION
 
@@ -11,8 +12,10 @@ ENV PATH="/opt/venv/bin:$PATH"
 RUN apk --update add --no-cache --virtual .build-deps unzip curl && \
     curl -sSL -o terraform.zip https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip && \
     curl -sSL -o /usr/bin/terragrunt https://github.com/gruntwork-io/terragrunt/releases/download/v${TERRAGRUNT_VERSION}/terragrunt_linux_amd64 && \
+    curl -sSL -o /usr/bin/get-poetry.py https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py && \
     unzip -o terraform.zip -d /usr/bin && rm terraform.zip && \
     chmod +x /usr/bin/terragrunt && \
+    python get-poetry.py --version ${POETRY_VERSION} && \
     apk add make bash && \
     apk del .build-deps && \
     pip install --upgrade pip
--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@ set -euo pipefail
 
 export DOCKER_BUILDKIT=1
 
-# GIT_COMMIT=$(git rev-parse --short HEAD)
 ECR_REPO=947967857684.dkr.ecr.eu-west-2.amazonaws.com
+POETRY_VERSION=$(cat .poetry-version)
 PYTHON_VERSION=$(cat .python-version)
 TERRAFORM_VERSION=$(cat .terraform-version)
 TERRAGRUNT_VERSION=$(cat .terragrunt-version)
 IMAGE_NAME=$ECR_REPO/iac-build-agent
-IMAGE_TAG="${PYTHON_VERSION}_${TERRAFORM_VERSION}_${TERRAGRUNT_VERSION}"
+IMAGE_TAG="${PYTHON_VERSION}_${POETRY_VERSION}_${TERRAFORM_VERSION}_${TERRAGRUNT_VERSION}"
 
 poetry update
 poetry export -o requirements.txt
@@ -18,10 +18,12 @@ aws ecr get-login-password --region eu-west-2 | docker login --username AWS --pa
 
 # Use branch+commit for tagging:
 docker build --tag "$IMAGE_NAME:$IMAGE_TAG" \
+             --build-arg POETRY_VERSION="$POETRY_VERSION" \
              --build-arg PYTHON_VERSION="$PYTHON_VERSION" \
              --build-arg TERRAFORM_VERSION="$TERRAFORM_VERSION" \
              --build-arg TERRAGRUNT_VERSION="$TERRAGRUNT_VERSION" \
              --build-arg BUILDKIT_INLINE_CACHE=1 \
+             --label poetry_version="$POETRY_VERSION" \
              --label python_version="$PYTHON_VERSION" \
              --label terraform_version="$TERRAFORM_VERSION" \
              --label terragrunt_version="$TERRAGRUNT_VERSION" .
